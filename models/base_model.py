04e9666825bfdce112b4158a9eeae3a66712370e
71Arid <brian87noel@gmail.com>
2023-05-15 18:04:52 +0300

    k

--- models/base_model.py
+++ models/base_model.py
@@ -13,13 +13,13 @@ class BaseModel:
         """ Initializes """
 
         if kwargs:
-            for key, value in kwargs.items():
-                if key != '__class__':
-                    setattr(self, key, value)
-                else:
-                    setattr(self, '_BaseModel__class', value)
-            self.created_at = datetime.strptime(self.created_at, '%Y-%m-%dT%H:%M:%S.%f')
-            self.updated_at = datetime.strptime(self.updated_at, '%Y-%m-%dT%H:%M:%S.%f')
+            for k, v in kwargs.items():
+                if k != "__class__":
+                    setattr(self, k, v)
+                if "created_at" in kwargs:
+                    self.created_at = datetime.strptime(kwargs["created_at"], '%Y-%m-%dT%H:%M:%S.%f')
+                if "updated_at" in kwargs:
+                    self.updated_at = datetime.strptime(kwargs["updated_at"], '%Y-%m-%dT%H:%M:%S.%f')
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
@@ -34,7 +34,7 @@ class BaseModel:
         storage.save()
 
     def to_dict(self):
-        dict_copy = self.__dict__.copy()
+        dict_copy = dict(self.__dict__)
         dict_copy['__class__'] = self.__class__.__name__
         dict_copy['created_at'] = self.created_at.isoformat()
         dict_copy['updated_at'] = self.updated_at.isoformat()
