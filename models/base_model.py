a96419eecf884117aad5b73b7767ec6fb4e62dca
71Arid <brian87noel@gmail.com>
2023-05-15 09:52:36 +0300

    new

--- models/base_model.py
+++ models/base_model.py
@@ -11,28 +11,25 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         """ initializes instances """
 
-        if kwargs:
-            for key, value in kwargs.item():
-                if key != '__class__':
-                    setattr(self, key, value)
-                else:
-                    setattr(self, '_BaseModel__class', value)
-            self.created_at = datetime.strptime(self.created_at, '%Y-%m-%dT%H:%M:%S.%f')
-            self.updated_at = datetime.strptime(self.updated_at, '%Y-%m-%dT%H:%M:%S.%f')
+                if kwargs:
+            self.__dict__.update(**kwargs)
+            self.created_at = datetime.fromisoformat(kwargs["created_at"])
+            self.updated_at = datetime.fromisoformat(kwargs["updated_at"])
         else:
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+            self.created_at = datetime.utcnow()
+            self.updated_at = self.created_at
 
     def __str__(self):
-        return '[{}] ({}) {}'.format(self.__class__.__name__, self.id, self.__dict__)
+        return f"[{type(self).__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
-        self.updated_at = datetime.now()
+        self.updated_at = datetime.utcnow()
 
     def to_dict(self):
         dict_copy = self.__dict__.copy()
-        dict_copy['__class__'] = self.__class__.__name__
-        dict_copy['created_at'] = self.created_at.isoformat()
-        dict_copy['updated_at'] = self.updated_at.isoformat()
+        dict_copy["created_at"] = dict_copy["created_at"].isoformat()
+        dict_copy["updated_at"] = dict_copy["updated_at"].isoformat()
+        dict_copy["__class__"] = type(self).__name__
+        dict_copy.pop("_sa_instance_state", None)
         return dict_copy
